f752e85b96c1fe6e1b4d4bf57afcb5b6919fb3bc
Fieraol <55558071+Fieraol@users.noreply.github.com>
2023-04-20 00:03:01 -0400

    script and carousel - fieraol

---
+++ script.js
@@ -0,0 +1,44 @@
+let slidePosition = 0;
+
+const slides = document.querySelectorAll('.carousel_item');
+
+const slidesArray = Array.from(slides);
+
+const totalSlides = slidesArray.length;
+
+function updateSlidePosition() {
+  slidesArray.forEach((slide) => {
+    slide.classList.remove('visible')
+    slide.classList.add('hidden')
+  });
+
+  slides[slidePosition].classList.add('visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+
+  updateSlidePosition();
+}
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition = slidePosition - 1;
+  }
+  updateSlidePosition();
+}
+
+document.querySelector('.next')
+  .addEventListener('click', () => {
+    moveToNextSlide();
+  });
+
+document.querySelector('.prev').addEventListener('click', () => {
+  console.log('clicked next');
+  moveToPrevSlide();
+});
